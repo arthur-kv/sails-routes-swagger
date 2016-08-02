9905863fba03678c65b035a48e8cacc43013474e
arthur-kv <arthur@MacBook-Pro-Arthur.local>
2016-08-02 10:37:31 -0400

    feat: breaking changes:  1. Generate paths objects for all the routes and for specified projects separately  2. Do not expose ouput swagger doc to sails.hook variable  3. Write all created swagger docs to json files (json file name is a project name)  4. Hooks route: removed /swagger/ui route created /swagger/ui/:project route instead, which returns .json file for specified project name

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const 
 	SwaggerParser 	= require('swagger-parser'),
+	fs 				= require('mz/fs'),
+	path 			= require('path'),
+	mime 			= require('mime'),
 	docGen 			= require('./lib/doc-generator');
 
 const 
@@ -13,32 +16,42 @@ module.exports = function sailsRoutesSwagger (sails) {
 
     return {
         initialize: function (cb) {
-        	configKey = this.configKey;
-            sails.after('router:after', () => {
-                let pathResults = docGen.generateSwaggerPathObjects(sails.config.routes);
 
+        	configKey = this.configKey;
+
+        	let _log = sails.log.verbose.bind(null, `${configKey}`);
+
+            sails.after('router:after', () => {
                 let swaggerConfig = sails.config[this.configKey];
 
-                let swaggerJSON = {
-                    swagger 		 	: SWAGGER_VER,
-                    info 			 	: docGen.convertPackageToSwagger(swaggerConfig.package),
-                    host 			 	: swaggerConfig.host || DEFAULT_HOST,
-                    tags 			 	: docGen.mergeTags(swaggerConfig.tags || [], pathResults.tags),
-                    definitions 	 	: swaggerConfig.definitions || {},
-                    securityDefinitions : swaggerConfig.securityDefinitions || {},
-                    paths 				: pathResults.paths,
-                    externalDocs 		: swaggerConfig.externalDocs || { url: DEFAULT_HOST }
+                let projectDocs = {
+                	all: generateSwaggerDoc(sails.config.routes, swaggerConfig)
                 };
 
-                /*
-				* Docs: https://github.com/BigstickCarpet/swagger-parser/blob/master/docs/swagger-parser.md#api
-                */
-                SwaggerParser.validate(swaggerJSON, (err, api) => {
-                	if(err) {
-                		throw err;
-                	} else {
-                		sails.hooks[this.configKey] = api;
+                _log('Generated swagger json for the whole project');
+
+                if (swaggerConfig.projects && swaggerConfig.projects.length) {
+                	swaggerConfig.projects.reduce((docs, projectConfig) => {
+                		docs[projectConfig.name] = generateSwaggerDoc(sails.config.routes, swaggerConfig, projectConfig)
+                		_log(`Generated swagger json for "${projectConfig.name}" subproject`);
+                		return docs;
+                	}, projectDocs);
+                }
+
+                _log('Validating generated swagger docs');
+
+                Promise.all(
+                	Object.keys(projectDocs).map(prKey => {
+                		return validateSwaggerDoc(projectDocs[prKey])
+                	})
+                ).then(() => {
+                	if(swaggerConfig.docsFolder) {
+                		_log('Writing json files');
+                		return writeSwaggerDocs(_log, projectDocs, swaggerConfig.docsFolder);
                 	}
+                }).catch(err => {
+                	sails.log.error(err);
+                	throw err;
                 });
             });
 
@@ -46,10 +59,80 @@ module.exports = function sailsRoutesSwagger (sails) {
         },
         routes: {
             after: {
-                'GET /swagger/ui': (req, res) => {
-                    res.status(200).json(sails.hooks[configKey]);
+                'GET /swagger/ui/:project': (req, res) => {
+                	let destinationDir = sails.config[configKey].docsFolder;
+
+                	if(!destinationDir) {
+                		res.statusCode = 500;
+                		res.end('Docs folder is not specified');
+                		return;
+                	}
+                	
+                	let filepath = path.resolve(destinationDir, `${req.params.project}.json`);
+
+					let rStream = fs.createReadStream(filepath);
+
+  					rStream.pipe(res);
+
+  					rStream.on('error', err => {
+  						if(err.code == 'ENOENT') {
+  							res.statusCode = 404;
+  							res.end('Not Found');
+  						} else {
+  							sails.log.error(err);
+  							if(!res.headersSent) {
+  								res.statusCode = 500;
+  								res.end('Internal Error');
+  							} else {
+  								res.end();
+  							}
+  						}
+  					}).on('open', () => {
+  						res.setHeader('Content-Type', mime.lookup(filepath));
+  					});
+
+  					res.on('close', () => {
+				    	rStream.destroy();
+				    });
                 }
             }
         }
     };
+}
+
+function generateSwaggerDoc (routes, swaggerConfig, projectConfig) {
+	let pathResults = docGen.generateSwaggerPathObjects(routes, projectConfig && projectConfig.name);
+
+    return {
+        swagger 		 	: SWAGGER_VER,
+        info 			 	: Object.assign(
+        							projectConfig && projectConfig.info || {}, 
+        							docGen.convertPackageToSwagger(swaggerConfig.package)
+        					  ),
+        host 			 	: swaggerConfig.host || DEFAULT_HOST,
+        tags 			 	: docGen.mergeTags(swaggerConfig.tags || [], pathResults.tags),
+        definitions 	 	: swaggerConfig.definitions || {}, // should I place definitions under project config either?
+        securityDefinitions : swaggerConfig.securityDefinitions || {},
+        paths 				: pathResults.paths,
+        externalDocs 		: swaggerConfig.externalDocs || { url: DEFAULT_HOST }
+    };
+}
+
+function validateSwaggerDoc (doc) {
+	/*
+	* Docs: https://github.com/BigstickCarpet/swagger-parser/blob/master/docs/swagger-parser.md#api
+    */
+	return SwaggerParser.validate(doc);
+}
+
+function writeSwaggerDocs (log, projects, dest) {
+	return Object.keys(projects).map(prName => {
+		let filepath = path.resolve(dest, `${prName.toLowerCase().replace(' ', '_')}.json`);
+
+		log('Writing', filepath);
+
+		return fs.writeFile(filepath, JSON.stringify(projects[prName], null, '  '), {
+			encoding: 'utf8'
+		})
+	});
 }
